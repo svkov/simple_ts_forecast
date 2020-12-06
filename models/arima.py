d2c39b52e131e3906784cfe11d0e9738c8d6730a
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-12-06 21:06:42 +0300

    fix arima

--- models/arima.py
+++ models/arima.py
@@ -10,30 +10,39 @@ warnings.filterwarnings("ignore")
 
 class ARIMA(Model):
 
-    def __init__(self, df, n=14, column_name='price', verbose=False, **kwargs):
+    def __init__(self, df, n=14, column_name='price', exog_names=None, verbose=False, **kwargs):
         super().__init__(df, n=n, verbose=verbose)
+        self.exog_names = exog_names
         self.column_name = column_name
-        self.model = fit_model(df.dropna(), self.n, self.column_name)
+        self.model = fit_model(df.dropna(), self.n, self.column_name, self.exog_names)
 
     def predict(self, df):
-        return predict_model(self.model, df, self.n, self.column_name)
+        return predict_model(self.model, df, self.n, self.exog_names)
 
     def predict_for_report(self, df, date_start, date_end):
-        return arima_predict_for_report(df.dropna(), date_start, date_end, self.n, self.column_name)
+        return arima_predict_for_report(df.dropna(), date_start, date_end, self.n, self.column_name, self.exog_names)
 
 
-def fit_model(pivoted_df, n, column_name):
+def fit_model(pivoted_df, n, column_name, exog_names):
     train = pivoted_df.iloc[:-n]
-    train_ex = train.drop([column_name], axis=1)
-    model = pmd.auto_arima(train[column_name], train_ex)
+    # train_ex = train.drop([column_name], axis=1)
+    if exog_names:
+        train_ex = train[exog_names]
+        model = pmd.auto_arima(train[column_name], train_ex)
+    else:
+        model = pmd.auto_arima(train[column_name])
     return model
 
 
-def predict_model(model, pivoted_df, n, column_name):
-    train = pivoted_df.iloc[:-n]
-    last_val = train.drop([column_name], axis=1).values[-1].reshape(1, -1).tolist()
-    last_ex = last_val * n
-    return model.predict(n, last_ex)
+def predict_model(model, pivoted_df, n, exog_names):
+    if exog_names:
+        train = pivoted_df.iloc[:-n]
+        # last_val = train.drop([column_name], axis=1).values[-1].reshape(1, -1).tolist()
+        last_val = train[exog_names].values[-1].reshape(1, -1).tolist()
+        last_ex = last_val * n
+        return model.predict(n, last_ex)
+    else:
+        return model.predict(n)
 
 
 def update_model(model, pivoted_df, n, column_name):
@@ -44,16 +53,16 @@ def update_model(model, pivoted_df, n, column_name):
     return model
 
 
-def arima_predict_for_report(df, start_date, end_date, n, column_name):
+def arima_predict_for_report(df, start_date, end_date, n, column_name, exog_names):
     pivoted_df = df[:start_date]
 
-    model = fit_model(pivoted_df, n, column_name)
+    model = fit_model(pivoted_df, n, column_name, exog_names)
     predictions = []
 
     for pivot in pd.date_range(start_date, end_date):
         pivoted_df = df[:pivot]
         model = update_model(model, pivoted_df, n, column_name)
-        prediction = predict_model(model, pivoted_df, n, column_name)
+        prediction = predict_model(model, pivoted_df, n, exog_names)
         predictions.append(prediction)
 
     columns = [f'{column_name} n{i + 1}' for i in range(n)]
